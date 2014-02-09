aea0761ce404442859407a2f017494c3e5c7cd19
allthesignals <wilburnforce@gmail.com>
2014-02-08 21:44:14 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lwc::Application.config.secret_token = '8e5b73faa02b46eac314cca1275419a09dc4689bef90410f3019cc84afdbd710694cc01fa3b4d04d169a230c16ad0f0a1f6a724f7f061267df16846e39575439'
