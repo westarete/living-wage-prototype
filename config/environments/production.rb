2fc7cf03272d039d006204aceae8d84188e4a76d
allthesignals <wilburnforce@gmail.com>
2014-04-02 18:46:06 -0400

    Trying production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Lwc::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
-
+  config.ember.variant = :development
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
