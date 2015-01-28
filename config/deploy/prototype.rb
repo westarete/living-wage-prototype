8541cc8cca38f8ea6747167bf6fc4e59438d523d
Scott Woods <scott@westarete.com>
2015-01-28 06:40:11 -0500

    Deploy to "prototype", not "beta"
    We're putting this app on the sidelines and primary development will
    occur on the existing app.

--- config/deploy/prototype.rb
+++ config/deploy/prototype.rb
@@ -1,9 +1,9 @@
 # The directory that we're deploying to on the remote host.
-set :deploy_to, "/var/www/sites/beta.livingwage.westarete.com"
+set :deploy_to, "/var/www/sites/prototype.livingwage.westarete.com"
 
 # Tell capistrano to use the beta environment. This is key for running 
 # the database migrations via "cap beta deploy:migrations".
-set :rails_env, "beta"
+set :rails_env, "prototype"
 
 # The hosts that we're deploying to.
 role :app, "livingwage.westarete.com"
